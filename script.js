2575a3b527fd4fa03cd0fbfaf99c1ab679ea1bbf
Shuo <186079699+ShuoWu6529@users.noreply.github.com>
2025-07-14 02:30:21 -0400

    Finish script for coloring grid

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function makeGrid(length) {
     const container = document.querySelector(".container")
     const docFrag = document.createDocumentFragment();
+
     for (let i = 0; i < length ** 2; i++) {
         const cell  = document.createElement("div");
         cell.classList.add("cell");
@@ -10,6 +11,17 @@ function makeGrid(length) {
     container.appendChild(docFrag);
     cellSize = 100 / length
     document.documentElement.style.setProperty(`--cell-size`, `${cellSize}%`)
+
+
+    container.addEventListener("mouseover", (e) => {
+        const target = e.target;
+        if (target.className === "container") return;
+
+        if (e.buttons === 1) {
+            target.classList.add("colored");
+        }
+    })
 }
+
 
 makeGrid(16);
